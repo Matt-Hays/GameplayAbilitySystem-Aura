e8789864c9e15610df00402ed88ea8ab50953c7e
MattHays <mkhays01@gmail.com>
2025-09-26 15:06:22 -0600

    Add child character and enemy classes

---
+++ Aura/Source/Aura/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,5 @@
+// Copyright Mattman
+
+
+#include "Character/AuraCharacter.h"
+
---
+++ Aura/Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,15 @@
+// Copyright Mattman
+
+
+#include "Character/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
