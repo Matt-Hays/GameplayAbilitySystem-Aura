1b2994962fffd547b65a5901ff91292fcb2ba397
MattHays <mkhays01@gmail.com>
2025-09-27 02:19:43 -0600

    Add camera and game mode

--- Aura/Source/Aura/Private/Character/AuraCharacter.cpp
+++ Aura/Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,17 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	UCharacterMovementComponent* CharacterMovementComponent = GetCharacterMovement();
+	CharacterMovementComponent->bOrientRotationToMovement = true;
+	CharacterMovementComponent->RotationRate = FRotator(0.f, 400.f, 0.f);
+	CharacterMovementComponent->bConstrainToPlane = true;
+	CharacterMovementComponent->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
